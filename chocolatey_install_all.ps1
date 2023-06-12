c169ed9bab091eeb3731ce9acfb8eac2ee64e74f
SmolSoft <smolsoft@email.cz>
2023-06-12 18:05:01 +0200

    cleaning

--- chocolatey_install_all.ps1
+++ chocolatey_install_all.ps1
@@ -1,33 +1,33 @@
-# internet 
+## internet 
 choco install firefox --params "/NoDesktopShortcut /NoAutoUpdate" -y
 choco install thunderbird --params "/NoDesktopShortcut /NoAutoUpdate" -y
 
-#Cloud
+# cloud
 choco install dropbox -y
 choco install megasync -y
 choco install pcloud -y
 
-# tools
+## tools
 choco install 7zip.install --params "/NoContextMenu" -y
 choco install freefilesync -y
-
 # choco install totalcommander --params '/NoDesktopIcon' -y
 choco install doublecmd --params "/NoDesktopIcon" -y
 choco install dupeguru -y
 choco install winscp -y
+
 choco install powertoys -y
 choco install bulk-crap-uninstaller -y
 choco install monitorian -y
 choco install sharex -y
 
 # development
-choco install notepadplusplus  -y
 choco install git --version -y
 choco install gitextensions.portable -y
-choco install vscode -y
 choco install powershell-core - --install-arguments='"DISABLE_TELEMETRY=1"' -y
 
-# IDEs
+# IDEs & editors
+choco install notepadplusplus  -y
+choco install vscode -y
 choco install intellijidea-community -y
 choco install androidstudio -y
 
