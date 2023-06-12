57dac1ed6075229b55deb1cdee5b9131cdb5dba8
SmolSoft <smolsoft@email.cz>
2023-06-12 15:20:17 +0200

    Working UI  - checkboxes generated dynamically from choco install list

--- ui/form.ps1
+++ ui/form.ps1
@@ -3,7 +3,7 @@ Add-Type -AssemblyName System.Windows.Forms
 # Create a new form
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "Chocolatey Install"
-$form.Size = New-Object System.Drawing.Size(300, 200)
+$form.Size = New-Object System.Drawing.Size(300, 400)
 $form.FormBorderStyle = 'FixedDialog'
 $form.MaximizeBox = $false
 
@@ -14,14 +14,26 @@ $label.Size = New-Object System.Drawing.Size(280, 20)
 $label.Text = "Select packages to install:"
 $form.Controls.Add($label)
 
-# Create checkboxes
+# Create checkboxes dynamically based on the content of the script
 $checkBoxes = @()
-$packages = @("package1", "package2", "package3")  # Add your package names here
+$packages = @()
+$scriptPath = Join-Path -Path $PSScriptRoot -ChildPath "..\chocolatey_install_all.ps1"  # Set the correct relative path to the base script
+
+# Read the content of the base script
+$scriptContent = Get-Content -Path $scriptPath
+
+# Parse the content and extract package names
+foreach ($line in $scriptContent) {
+    if ($line -match "choco install ([^\s]+)") {
+        $packageName = $matches[1]
+        $packages += $packageName
+    }
+}
 
 for ($i = 0; $i -lt $packages.Count; $i++) {
     $checkBox = New-Object System.Windows.Forms.CheckBox
-    $checkBox.Location = New-Object System.Drawing.Point(20, 50 + $i * 25)
-    $checkBox.Size = New-Object System.Drawing.Size(200, 20)
+    $checkBox.Location = [System.Drawing.Point]::new(20, 50 + $i * 25)
+    # $checkBox.Size = New-Object System.Drawing.Size(200, 20)
     $checkBox.Text = $packages[$i]
     $checkBoxes += $checkBox
     $form.Controls.Add($checkBox)
@@ -29,7 +41,7 @@ for ($i = 0; $i -lt $packages.Count; $i++) {
 
 # Create the "OK" button
 $okButton = New-Object System.Windows.Forms.Button
-$okButton.Location = New-Object System.Drawing.Point(100, 150)
+$okButton.Location = New-Object System.Drawing.Point(100, 350)
 $okButton.Size = New-Object System.Drawing.Size(75, 23)
 $okButton.Text = "OK"
 $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
